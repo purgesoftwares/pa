b476870df143e4625ed0d40275442b921341574e
Shankar Palsaniya <shankar.dayal@purgesoftwares.com>
2017-04-18 13:39:25 +0530

    small fix

--- src/app/provider-forgot-password/index.ts
+++ src/app/provider-forgot-password/index.ts
@@ -1,5 +1,8 @@
 /**
  * This barrel file provides the export for the lazy loaded AboutComponent.
  */
+export * from './forgotPassword.component';
+export * from './forgotPassword.routes';
+
 export * from './providerforgotPassword.component';
 export * from './providerforgotPassword.routes';
